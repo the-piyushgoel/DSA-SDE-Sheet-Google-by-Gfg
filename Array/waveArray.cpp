f79119c17ff233c0ccee186fc73d512a32e8f80c
Piyush Goel <piyush.goel2704@gmail.com>
2026-01-31 22:36:08 +0530

    Wave function

--- Array/waveArray.cpp
+++ Array/waveArray.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-void sortInWave(vector<int> &arr)
-{
-    for (int i = 1; i < arr.size(); i += 2)
-    {
+void sortInWave(vector<int> &arr){
+    for (int i = 1; i < arr.size(); i += 2){
         swap(arr[i], arr[i - 1]);
     }
 }
